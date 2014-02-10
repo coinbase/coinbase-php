94dc54383ffbcbbd170904205bd5b5d4947a93c8
Isaac Waller <isaac@siriusapplications.com>
2014-02-10 15:00:46 -0800

    Composer fixes

--- lib/Coinbase.php
+++ lib/Coinbase.php
@@ -4,11 +4,11 @@ if(!function_exists('curl_init')) {
     throw new Exception('The Coinbase client library requires the CURL PHP extension.');
 }
 
-require(dirname(__FILE__) . '/Coinbase/Exception.php');
-require(dirname(__FILE__) . '/Coinbase/ApiException.php');
-require(dirname(__FILE__) . '/Coinbase/ConnectionException.php');
-require(dirname(__FILE__) . '/Coinbase/Coinbase.php');
-require(dirname(__FILE__) . '/Coinbase/Requestor.php');
-require(dirname(__FILE__) . '/Coinbase/Rpc.php');
-require(dirname(__FILE__) . '/Coinbase/OAuth.php');
-require(dirname(__FILE__) . '/Coinbase/TokensExpiredException.php');
+require_once(dirname(__FILE__) . '/Coinbase/Exception.php');
+require_once(dirname(__FILE__) . '/Coinbase/ApiException.php');
+require_once(dirname(__FILE__) . '/Coinbase/ConnectionException.php');
+require_once(dirname(__FILE__) . '/Coinbase/Coinbase.php');
+require_once(dirname(__FILE__) . '/Coinbase/Requestor.php');
+require_once(dirname(__FILE__) . '/Coinbase/Rpc.php');
+require_once(dirname(__FILE__) . '/Coinbase/OAuth.php');
+require_once(dirname(__FILE__) . '/Coinbase/TokensExpiredException.php');
